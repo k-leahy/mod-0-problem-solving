667af04b64aee5b794b5040ca7f496bad792262f
Kelly <119141863+k-leahy@users.noreply.github.com>
2023-01-06 11:29:22 -0700

    Add challenge

---
+++ challenge2.rb
@@ -0,0 +1,16 @@
+# Challenge 2
+# Start with an array of strings. Then print only the elements in lowercase letters.
+# An array of stings --> printing each string to the console
+# Create an array of stings with each element having both uppercase and lowercase letters.
+# Iterate through the array using .each Method
+# All letters will print in only lowercase. If the letter was uppercase it will now be lowercase. All elements will print.
+
+teachers = ["Mrs, Smith", "Mr. Ellis", "Mrs. King", "Mrs. Tillis"]
+
+teachers.each do |teacher|
+   p teacher.downcase
+end
+
+
+# The purpose of this method is to take every letter in each element and make it lowercase
+# The final solution from this method is mrs. smith mr. ellis mrs. king mrs. tillis
